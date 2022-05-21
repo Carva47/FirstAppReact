b51a533e6e3814a1b5a32c9d2fedde3e7eee79b5
Carva47 <nvualacanvula@gmail.com>
2022-05-21 02:07:34 +0100

    Alterando o scr/app.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
 
+function MyButton () {
+  return(
+    <button> Clica-me</button>
+  
+  );
+}
+const user = {
+  name: 'Hedy Lamarr',
+  imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+  imageSize: 90,
+};
+
+function Profile () {
+  return (
+    <>
+      <h1>{user.name}</h1>
+      <img
+        className="avatar"
+        src={user.imageUrl}
+        alt={'Photo of ' + user.name}
+        style={{
+          width: user.imageSize,
+          height: user.imageSize,
+          borderRadius: user.imageSize
+        }}
+      />
+    </>
+  );
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <h1> Bem-vindo a minha aplicação</h1>
+       <MyButton/>
+       <Profile/>
+      </div>     
   );
 }
 
